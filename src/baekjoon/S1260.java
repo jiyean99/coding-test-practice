e9d8b1ed2221ce3defb462137532a620e4d35a50
이지연 <wldus019113@icloud.com>
2025-12-29 19:43:04 +0900

    - DFS/BFS - 연결요소의 개수

--- src/baekjoon/S1260.java
+++ src/baekjoon/S1260.java
@@ -3,9 +3,7 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 // DFS와 BFS
 public class S1260 {
